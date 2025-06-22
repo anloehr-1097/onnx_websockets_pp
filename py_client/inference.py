a9943df9b2ac16e59b2bee18950ff8340b91ffff
Andreas Loehr <andreas.loehr97@gmail.com>
2025-06-22 19:58:04 +0200

    cleanup

--- py_client/inference.py
+++ py_client/inference.py
@@ -1,7 +1,6 @@
 import onnxruntime
 from pathlib import Path
 from PIL import Image
-import onnx
 import numpy
 
 
@@ -12,8 +11,8 @@ def load_model(model_name: str):
     input_name = session.get_inputs()[0].name
     input_shape = session.get_inputs()[0].shape
     output_name = session.get_outputs()[0].name
-    print('Input Name:', input_name)
-    print('Output Name:', output_name)
+    print("Input Name:", input_name)
+    print("Output Name:", output_name)
     return session, input_name, input_shape, output_name
 
 
@@ -21,11 +20,12 @@ def load_image(im_path: Path, input_shape) -> numpy.ndarray:
     image: Image.Image = Image.open(im_path).convert("RGB")
     image = image.resize(input_shape[2:])
     return numpy.asarray(image)
-        
+
+
 def preprocess_image(img: numpy.ndarray) -> numpy.ndarray:
     img = img.astype(numpy.float32)
     img /= 255
-    img = img.transpose((2,0,1))
+    img = img.transpose((2, 0, 1))
     img = img[None, :]
     return img
 
@@ -49,6 +49,7 @@ def main():
     pred = postprocess_onnx_out(out)
     print(pred)
     return
+
 
 if __name__ == "__main__":
     main()
